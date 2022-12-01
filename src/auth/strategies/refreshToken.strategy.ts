59f6cb96ca78888a4c34a8a07b7fa02dac5f113d
farrellh1 <farrell.hauzan@gmail.com>
2022-12-01 22:17:28 +0700

    add auth strategies

---
+++ src/auth/strategies/refreshToken.strategy.ts
@@ -0,0 +1,27 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
+import { ForbiddenException } from '@nestjs/common';
+
+export class RefreshTokenStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.REFRESH_SECRET,
+      passReqToCallback: true,
+    });
+  }
+
+  validate(req: Request, payload: any) {
+    const refreshToken = req
+      ?.get('authorization')
+      ?.replace('Bearer', '')
+      .trim();
+
+    if (!refreshToken) throw new ForbiddenException('Refresh token malformed');
+    return { ...payload, refreshToken };
+  }
+}
