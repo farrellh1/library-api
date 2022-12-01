76353513395db43c5fb0eee3c0c055b043099892
farrellh1 <farrell.hauzan@gmail.com>
2022-12-01 11:16:22 +0700

    Fix Prisma module

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,10 +1,9 @@
 import { Global, Module } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
 import { PrismaService } from './prisma.service';
 
 @Global()
 @Module({
-  providers: [PrismaClient],
+  providers: [PrismaService],
   exports: [PrismaService],
 })
-export class AppModule {}
+export class PrismaModule {}
