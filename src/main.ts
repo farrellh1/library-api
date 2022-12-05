778adbced383d6f4dda001dcc0ef602bb9090454
farrellh1 <farrell.hauzan@gmail.com>
2022-12-05 16:04:46 +0700

    Add Expection and Serializer in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,31 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger API
   const config = new DocumentBuilder()
     .setTitle('Library API')
     .setDescription('The Library API documentation')
     .setVersion('1.0')
-    .addTag('library')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.useGlobalPipes(new ValidationPipe())
+
+  // Validation
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Prisma Error Exception
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
+  // Serializer
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   await app.listen(3000);
 }
 bootstrap();
