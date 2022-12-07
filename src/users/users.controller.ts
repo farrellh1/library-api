0c23139235d73e07aec10dd96c184740a08dc821
farrellh1 <farrell.hauzan@gmail.com>
2022-12-07 15:16:23 +0700

    add profile endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,6 +11,7 @@ import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { UserEntity } from './entities/user.entity';
+import { CurrentUserId } from 'src/common/decorators';
 
 @ApiTags('User')
 @Controller('users')
@@ -25,17 +26,20 @@ export class UsersController {
     return users.map((user) => new UserEntity(user));
   }
 
-  @Get(':id')
+  @Get('profile')
   @ApiBearerAuth()
   @ApiOkResponse({ type: UserEntity })
-  async findOne(@Param('id') id: string) {
-    return new UserEntity(await this.userService.findOne(+id));
+  async findOne(@CurrentUserId() id: number) {
+    return new UserEntity(await this.userService.findOne(id));
   }
 
-  @Patch(':id')
+  @Patch('profile')
   @ApiBearerAuth()
-  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return new UserEntity(await this.userService.update(+id, updateUserDto));
+  async update(
+    @CurrentUserId() id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return new UserEntity(await this.userService.update(id, updateUserDto));
   }
 
   @Delete(':id')
